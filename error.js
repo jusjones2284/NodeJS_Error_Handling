73d6603dff5d5b966848f28682d4f5894f0f3e89
Justin Jones <jus.jones@yahoo.com>
2024-04-29 17:15:56 -0500

    updating error handling

---
+++ error.js
@@ -0,0 +1,61 @@
+//Error Object
+const error = new Error("Something went wrong!")
+// console.log(error.stack);
+// console.log(error.message);
+
+// throw new Error("I am error object");
+
+const {CustomError} = require("./CustomError")
+
+// throw new CustomError("This is a custom error object")
+
+
+// try{ 
+//     doSomething()
+// } catch(e){
+//     console.log("second error occured");
+//     console.log(e)
+// }
+
+
+function doSomething(){
+    // const data = fetch("localhost:300/api")
+    console.log("I am from doSomethingFunction");
+    const data = "I am from doSomethingFunction"
+    return data; 
+}
+
+// //Uncaught Exception
+// process.on("uncaughtException", ()=>{
+//     console.log("There was an uncaught exception");
+//     process.exit(1);
+// })
+
+// doSomething()
+
+const promise = new Promise((resolve, reject)=>{
+    if(false){
+        resolve(doSomething());
+    } else {
+        reject(doSomething())
+    }
+});
+
+promise
+    .then((val)=> {
+        console.log(val);
+    })
+    .catch((err) =>{
+        console.log("Error Occured");
+        console.log(err);
+    })
+
+
+
+
+// try{
+//     doSomething()
+// } catch(e){
+//     console.log("Error Occured");
+//     console.log(e)
+// };
